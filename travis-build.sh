d62ced0a835bd401e5352dfbc9e66a754607522b
Casey Getz <cgetz@linkedin.com>
2020-01-20 17:49:26 -0800

    Test sonarcloud
    yo

--- travis-build.sh
+++ travis-build.sh
@@ -4,15 +4,12 @@
 set -e
 
 echo "Building and testing artifacts, and creating pom files"
-./gradlew -s --scan build publishToMavenLocal codeCoverageReport
+./gradlew -s --scan build intTest codeCoverageReport sonarqube publishToMavenLocal
 
-echo "Testing Bintray publication by uploading in dry run mode"
-./gradlew -s -i --scan bintrayUploadAll -Pbintray.dryRun
-
-echo "Pull request: [$TRAVIS_PULL_REQUEST], Travis branch: [$TRAVIS_BRANCH]"
-# release only from master when no pull request build
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
-then    
-    echo "Releasing (tagging, uploading to Bintray)"
-    ./gradlew -s -i --scan ciPerformRelease
-fi
+#echo "Pull request: [$TRAVIS_PULL_REQUEST], Travis branch: [$TRAVIS_BRANCH]"
+## release only from master when no pull request build
+#if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
+#then
+#    echo "Releasing (tagging, uploading to Bintray)"
+#    ./gradlew -s -i --scan ciPerformRelease
+#fi
